a47c02e907dee9c86c700072b1321db16a708ed8
Sanjeev Saroy <saroy94@gmail.com>
2018-04-21 12:00:11 +0100

    Converts RGB to Hex and also added button to copy it

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let hexValue;
+
 $('.randomise-btn').click(function() {
   getRandomColour();
 });
@@ -15,12 +17,22 @@ function getRandomColour() {
   }
 
   setColour(rgb);
+  convertRGBtoHex(rgb);
 }
 
 function setColour(rgb) {
 
   let cssValue = 'rgb('+rgb.red+','+rgb.green+','+rgb.blue+')';
   $('body').css('background-color', cssValue);
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function convertRGBtoHex(rgb) {
+  hexValue = componentToHex(rgb.red) + componentToHex(rgb.green) + componentToHex(rgb.blue);
 }
 
 // Set a random colour on load
