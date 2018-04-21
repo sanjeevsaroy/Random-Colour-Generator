8aa34328c556c8f49d70f664730bcd8437c82969
Sanjeev Saroy <saroy94@gmail.com>
2018-04-21 10:58:06 +0100

    Inverts text colour for each background colour change

--- app.js
+++ app.js
@@ -20,6 +20,20 @@ function getRandomColour() {
 function setColour(hex) {
   let colour = '#' + hex;
   $('body').css('background-color', colour);
+
+  let invertedColour = invertColour(colour);
+  $('h1').css('color', invertedColour);
+}
+
+function invertColour(hexTripletColour) {
+    var colour = hexTripletColour;
+    colour = colour.substring(1);           // remove #
+    colour = parseInt(colour, 16);          // convert to integer
+    colour = 0xFFFFFF ^ colour;             // invert three bytes
+    colour = colour.toString(16);           // convert to hex
+    colour = ("000000" + colour).slice(-6); // pad with leading zeros
+    colour = "#" + colour;                  // prepend #
+    return colour;
 }
 
 // Set a random colour on load
