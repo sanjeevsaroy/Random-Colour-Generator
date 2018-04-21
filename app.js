a34c36e28724fc2a7fc9d7fc77773c64cf2634ec
Sanjeev Saroy <saroy94@gmail.com>
2018-04-21 11:46:17 +0100

    Removed heading

--- app.js
+++ app.js
@@ -21,16 +21,6 @@ function setColour(rgb) {
 
   let cssValue = 'rgb('+rgb.red+','+rgb.green+','+rgb.blue+')';
   $('body').css('background-color', cssValue);
-
-  let textColour = getReadableColour(rgb);
-  $('h1').css('color', textColour);
-}
-
-function getReadableColour(rgb) {
-  let brightness = Math.round(((parseInt(rgb.red * 299)) + (parseInt(rgb.green * 587)) * (parseInt(rgb.blue * 114)) / 1000));
-  let textColour = (brightness < 1000000) ? 'white' : 'black';
-
-  return textColour;
 }
 
 // Set a random colour on load
